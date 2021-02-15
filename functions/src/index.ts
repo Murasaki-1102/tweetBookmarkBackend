eee5426e67e624fbcf33f91123e809257e80e956
haruki_murasaki <haruki_murasaki@haruki-murasakinoMacBook-Pro.local>
2021-02-15 22:13:32 +0900

    Add batch

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,52 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+import { Tag } from "./types/tag";
+const Twitter = require("twitter-lite");
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
+admin.initializeApp();
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello from Firebase!");
-});
+exports.requestToken = functions
+  .region("asia-northeast1")
+  .https.onCall(async (data, context) => {
+    const twitter = new Twitter({
+      consumer_key: functions.config().twitter.apikey,
+      consumer_secret: functions.config().twitter.apikeysecret,
+    });
+    const token = await twitter.getRequestToken(data.callbackUrl);
+    return { ...token };
+  });
+
+exports.onWriteTag = functions
+  .region("asia-northeast1")
+  .firestore.document("users/{userUid}/tags/{tagId}")
+  .onWrite(async (change, context) => {
+    const { userUid } = context.params;
+    const db = admin.firestore();
+    const batch = db.batch();
+    try {
+      const tagsDocRef = await db
+        .collection(`users/${userUid}/tags`)
+        .orderBy("index", "asc")
+        .get();
+      const fotmattedTags: Tag[] = tagsDocRef.docs.map((tag, index) => {
+        const data = tag.data();
+        return {
+          id: tag.id,
+          index,
+          name: data.name,
+          emoji: data.emoji,
+          tweets: data.tweets || [],
+          createdAt: data.createdAt.toDate(),
+        };
+      });
+      await tagsDocRef.docs.map((tag) => {
+        const afterIndex = fotmattedTags.find(
+          (formattedTag) => formattedTag.id === tag.id
+        )?.index;
+        batch.update(tag.ref, { index: afterIndex });
+      });
+      await batch.commit();
+    } catch (error) {
+      console.log(error);
+    }
+  });
---
+++ functions/src/types/tag.ts
@@ -0,0 +1,8 @@
+export type Tag = {
+  id: string;
+  index: number;
+  name: string;
+  emoji: string;
+  tweets: any[];
+  createdAt: Date;
+};
