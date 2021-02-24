6e76df96b534b8012fef311c81584bf7b6d60228
purpleeeee <h.purpleeeee@gmail.com>
2021-02-25 02:22:55 +0900

    Add getAllTweet

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,9 +2,20 @@ import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 import { unset } from "lodash";
 import { Tag } from "./types/tag";
+//tserrorがでるため
+// import Twitter from "twitter-lite";
 const Twitter = require("twitter-lite");
 
 admin.initializeApp();
+
+const getTwitterClient = (accessToken: string, accessTokenSecret: string) => {
+  return new Twitter({
+    consumer_key: functions.config().twitter.apikey,
+    consumer_secret: functions.config().twitter.apikeysecret,
+    access_token_key: accessToken,
+    access_token_secret: accessTokenSecret,
+  });
+};
 
 exports.requestToken = functions
   .region("asia-northeast1")
@@ -56,22 +67,55 @@ exports.onWriteTag = functions
     }
   });
 
-exports.onSetAllTweet = functions
+exports.getAllTweet = functions
   .region("asia-northeast1")
   .https.onCall(async (data, context) => {
     const uid = context.auth?.uid!;
     const db = admin.firestore();
     const batch = db.batch();
     const tweetRef = db.collection(`users/${uid}/tweets`);
-    const allTweet = data.tweets.map((tweet: any) =>
-      unset(tweet, "quoted_status.place.bounding_box.coordinates")
-    );
-    await allTweet.map(async (tweet: any) => {
-      batch.set(tweetRef.doc(tweet.id_str), {
-        tweet,
-        tagsId: [],
-        createdAt: new Date(),
+
+    const client = getTwitterClient(data.accessToken, data.accessTokenSecret);
+
+    try {
+      let newTweets = await client.get("favorites/list", {
+        tweet_mode: "extended",
       });
+
+      for (let tweet of newTweets) {
+        const doc = await tweetRef.doc(tweet.id_str).get();
+        if (!doc.exists) {
+          unset(tweet, "quoted_status.place.bounding_box.coordinates");
+          unset(tweet, "place.bounding_box.coordinates");
+          await batch.set(doc.ref, {
+            tweet,
+            tagsId: [],
+            createdAt: new Date(),
+          });
+        }
+      }
+      await batch.commit();
+    } catch (e) {
+      if ("errors" in e) {
+        // Twitter API error
+        if (e.errors[0].code === 88) {
+          // rate limit exceeded
+          console.log(
+            "Rate limit will reset on",
+            new Date(e._headers.get("x-rate-limit-reset") * 1000)
+          );
+        } else {
+          console.log(e);
+        }
+      } else {
+        console.log(e);
+      }
+    }
+
+    let allTweets: any[] = [];
+    (await tweetRef.orderBy("createdAt", "desc").get()).forEach((doc) => {
+      allTweets.push(doc.data().tweet);
     });
-    await batch.commit();
+
+    return { allTweets };
   });
